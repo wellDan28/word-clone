7dd0cb631e48829c560c5b38be130d5853368982
Dan Klausner <dan@melio.com>
2023-03-20 08:48:28 +0200

    new component

--- src/components/Guess/Guess.js
+++ src/components/Guess/Guess.js
@@ -1,34 +1,7 @@
 import React from "react";
 
-function Guess({ onGuess }) {
-  const [guess, setGuess] = React.useState("");
-
-  const handleGuessChange = (e) => {
-    setGuess(e.target.value.toUpperCase());
-  };
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    onGuess(guess);
-    setGuess("");
-  };
-
-  return (
-    <form className="guess-input-wrapper" onSubmit={handleSubmit}>
-      <label htmlFor="guess-input">Enter guess:</label>
-      <input
-        id="guess-input"
-        type="text"
-        value={guess}
-        onChange={handleGuessChange}
-        minLength={5}
-        maxLength={5}
-        pattern="[a-zA-Z]{5}"
-        title="5 letter word"
-        required
-      />
-    </form>
-  );
+function Guess() {
+  return <div />;
 }
 
 export default Guess;
